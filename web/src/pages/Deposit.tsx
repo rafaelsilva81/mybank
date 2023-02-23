2b0a4819a86eb78d680f5e20a77b66b89ce96bc8
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-02-23 13:31:28 -0300

    feat: deposit

---
+++ web/src/pages/Deposit.tsx
@@ -0,0 +1,82 @@
+import React, { useRef, useState } from 'react';
+import { useMutation, useQueryClient } from 'react-query';
+import { toast } from 'react-toastify';
+import LoadingElement from '../components/LoadingElement';
+import api from '../util/api';
+
+const Deposit = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement>(null);
+  const [loading, setLoading] = useState(false);
+  const queryClient = useQueryClient();
+
+  const sendDeposit = async () => {
+    const amount = inputRef.current?.value;
+    const description = descriptionRef.current?.value;
+
+    if (!amount) {
+      toast('Please enter an valid amount', { type: 'error' });
+      return;
+    }
+
+    setLoading(true);
+
+    await api
+      .post('/transactions/deposit', {
+        amount: parseFloat(amount),
+        description,
+      })
+      .catch((err) => {
+        toast(err.response.data.message, { type: 'error' });
+        return;
+      })
+      .then((res) => {
+        toast('Deposit sucessful', { type: 'success' });
+        // refetch a query
+        queryClient.invalidateQueries('user');
+      })
+      .finally(() => {
+        setLoading(false);
+        inputRef.current!.value = '';
+      });
+  };
+
+  if (loading) {
+    return <LoadingElement />;
+  }
+
+  return (
+    <div className='flex flex-col w-full h-screen items-center justify-center'>
+      {/* Simple box asking how much to deposit */}
+      <div className='flex flex-col w-96 gap-2 rounded-md p-6 shadow-md bg-indigo-700'>
+        <h3 className='text-lg font-bold text-white'> Deposit </h3>
+        <input
+          type='number'
+          className='border-2 border-gray-300 p-2 rounded-md'
+          required
+          placeholder='Amount'
+          ref={inputRef}
+          step='0.01'
+          min={0}
+          max={1000000}
+        />
+        <textarea
+          className='border-2 border-gray-300 p-2 rounded-md'
+          required
+          placeholder='Description (optional)'
+          ref={descriptionRef}
+          maxLength={200}
+        />
+
+        <button
+          className='bg-white text-indigo-700 p-2 rounded-md hover:bg-indigo-200'
+          onClick={sendDeposit}
+        >
+          Deposit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Deposit;
